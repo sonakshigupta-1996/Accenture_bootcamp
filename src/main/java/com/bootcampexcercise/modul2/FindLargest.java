3f778f940bac289af2d58fb1955565b14f11b060
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-07 15:13:36 +0300

    Largest value using if else

---
+++ src/main/java/com/bootcampexcercise/modul2/FindLargest.java
@@ -0,0 +1,15 @@
+package com.bootcampexcercise.modul2;
+
+public class FindLargest {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 89;
+        int z = 2;
+        if(x>y){
+            System.out.println("Largest value");
+        }
+        else{
+            System.out.println("Final largest value");
+        }
+    }
+}
