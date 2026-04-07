267d1c238421a00a81514272efabad70f32b9f94
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-07 14:17:39 +0300

    Variable assignment activity

---
+++ src/main/java/com/bootcampexcercise/modul2/VariableAssignmentActivity.java
@@ -0,0 +1,9 @@
+package com.bootcampexcercise.modul2;
+
+public class VariableAssignmentActivity {
+    public static void main(String[] args) {
+        int i = 20;
+        i= i+5;//updating value
+        System.out.println("Value of i: "+i);
+    }
+}
