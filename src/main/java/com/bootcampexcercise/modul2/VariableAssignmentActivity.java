fb14629b888039501a82a9f8279ef43a502205d2
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-07 14:22:39 +0300

    Variable assignment activity

--- src/main/java/com/bootcampexcercise/modul2/VariableAssignmentActivity.java
+++ src/main/java/com/bootcampexcercise/modul2/VariableAssignmentActivity.java
@@ -2,8 +2,12 @@ package com.bootcampexcercise.modul2;
 
 public class VariableAssignmentActivity {
     public static void main(String[] args) {
-        int i = 20;
-        i= i+5;//updating value
-        System.out.println("Value of i: "+i);
+        int i = 5;
+        int j; //declare j
+        j=7;
+        int sum = i+j;
+
+
+        System.out.println("sum of numbers: "+sum);
     }
 }
