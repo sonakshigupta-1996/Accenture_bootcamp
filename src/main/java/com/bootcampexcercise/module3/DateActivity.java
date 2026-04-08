afddb959181e35c5ac7dfc5b00fe6dba4581c577
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-08 11:13:55 +0300

    commit module 3- date activity

---
+++ src/main/java/com/bootcampexcercise/module3/DateActivity.java
@@ -0,0 +1,11 @@
+package com.bootcampexcercise.module3;
+import java.util.Date;
+public class DateActivity {
+    public static void main(String[] args) {
+        // Create instance of Date
+        Date d = new Date();
+
+        // Print today's date
+        System.out.println("Today's date: " + d);
+    }
+}
