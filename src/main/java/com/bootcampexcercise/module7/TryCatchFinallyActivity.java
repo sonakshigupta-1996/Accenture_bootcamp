89b2099179fb74a841b2f3296f9c7ae17de066f7
Sonakshi <sonakshigupta.135@gmail.com>
2026-04-09 11:32:57 +0300

    commit try and catch

---
+++ src/main/java/com/bootcampexcercise/module7/TryCatchFinallyActivity.java
@@ -0,0 +1,35 @@
+package com.bootcampexcercise.module7;
+
+public class TryCatchFinallyActivity {
+    public static void main(String[] args) {
+
+        // Call method
+        TryCatchFinallyActivity obj = new TryCatchFinallyActivity();
+        obj.catchMeIfYouCan();
+    }
+
+    void catchMeIfYouCan() {
+
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        // try block
+        try {
+
+            // i <= 10 will cause error (array size is 10 → last index is 9)
+            for (int i = 0; i <= 10; i++) {
+                System.out.println(arr[i]);
+            }
+
+        } catch (IndexOutOfBoundsException e) {
+
+            // custom message
+            System.out.println("Exception occurred: Array index is out of bounds!");
+
+        } finally {
+
+            // always executes
+            System.out.println("Finally block executed (runs always)");
+        }
+    }
+}
+
